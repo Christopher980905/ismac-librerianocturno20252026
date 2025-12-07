7be2113180c86666e0f0df5d73fcabe40055896a
Christopher980905 <christopherp@tecnologicoismac.edu.ec>
2025-12-07 16:18:44 -0500

    Service

--- src/main/java/com/distribuida/dao/ClienteRepository.java
+++ src/main/java/com/distribuida/dao/ClienteRepository.java
@@ -4,4 +4,5 @@ import com.distribuida.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClienteRepository extends JpaRepository<Cliente,Integer> {
+    Integer idCliente(int idCliente);
 }
--- src/main/java/com/distribuida/service/ClienteServiceImpl.java
+++ src/main/java/com/distribuida/service/ClienteServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClienteServiceImpl implements ClienteService {
@@ -20,8 +21,8 @@ public class ClienteServiceImpl implements ClienteService {
     }
 
     @Override
-    public Cliente findOne(int id) {
-        return null;
+    public Optional<Cliente> findOne(int id) {
+        return clienteRepository.findById(id);
     }
 
     @Override
@@ -30,12 +31,30 @@ public class ClienteServiceImpl implements ClienteService {
     }
 
     @Override
-    public Cliente update(Cliente cliente) {
-        return null;
+    public Cliente update(int id, Cliente cliente) {
+        Optional<Cliente> clienteExistente = clienteRepository.findById(id);
+
+                if (clienteExistente == null) {
+                    return null;
+                }
+
+                clienteExistente.orElse(null).setCedula(cliente.getCedula());
+                clienteExistente.orElse(null).setNombre(cliente.getNombre());
+                clienteExistente.orElse(null).setApellido(cliente.getApellido());
+                clienteExistente.orElse(null).setDireccion(cliente.getDireccion());
+                clienteExistente.orElse(null).setTelefono(cliente.getTelefono());
+                clienteExistente.orElse(null).setCorreo(cliente.getCorreo());
+
+                return clienteRepository.save(clienteExistente.orElse(null));
+
+
+
     }
 
     @Override
     public void delete(int id) {
-
+        if (clienteRepository.existsById(id)) {
+            clienteRepository.deleteById(id);
+        }
     }
 }
